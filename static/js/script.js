3b1fb12ebb40c7b27b87c1b09c8d5cb96f24ab82
Laxmi <laxmiprajapat221242@gmail.com>
2026-06-03 18:25:53 +0530

    update

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,13 @@ card.addEventListener("click",()=>{
     card.innerHTML = `
     <h1 class="text-lg mt-4">Best of Luck! </h1>`;
 });
+
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener(("click"),()=>{
+
+    mobileMenu.classList.toggle("hidden");
+})
+
+
